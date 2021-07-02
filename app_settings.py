bf8f04abbb005a1a4a6bfa54d853bdc823506c4f
bender321 <43913159+bender321@users.noreply.github.com>
2021-07-02 14:53:07 +0200

    Delete app_settings.py

--- app_settings.py
+++
@@ -1,6 +0,0 @@
-DEBUG = True
-SQLALCHEMY_ENGINE_OPTIONS = {'pool_size': 5}
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = 'postgresql://zgixadaz:' \
-                          'adAlYRX_qv8k-NBIyvLPzu8s8FksBLMP' \
-                          '@hattie.db.elephantsql.com/zgixadaz'
